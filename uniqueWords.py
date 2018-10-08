7637f66cc8ff539dc1ac448d7406523f013cc09e
Noah <adel_chicago@yahoo.com>
2018-10-07 20:30:49 -0500

    added a python file to print unique words

---
+++ uniqueWords.py
@@ -0,0 +1,36 @@
+
+def getWords(text):
+    words = []
+    currentWord = ""
+    for char in text:
+        if char == " ":
+            words.append(currentWord)
+            currentWord = ""
+        else:
+            currentWord += char
+    words.append(currentWord)
+    return words
+
+
+def getUinqueWords(sentence):
+    uniqueWords = []
+    words = getWords(sentence)
+    for word in words:
+        if word in uniqueWords:
+            continue
+        uniqueWords.append(word)
+    return uniqueWords
+
+
+testCase1 = "common is a word used to common to word to express similarities"
+result = getUinqueWords(testCase1)
+print(result)
+
+testCase2 = "word word word word word"
+result = getUinqueWords(testCase2)
+print(result)
+
+testCase2 = "word                          word"
+result = getUinqueWords(testCase2)
+print(result)
+
