76ef238d2790d2e40e9037ea6e063106e6a1773e
Noah <adel_chicago@yahoo.com>
2018-10-11 09:06:17 -0500

    Count vowels in a text

---
+++ VowelsCount.py
@@ -0,0 +1,14 @@
+def VowelsCount(text):
+
+        vowel = ["a" , "e", "o", "i","u"]
+        for i in range (0 , len(vowel)) :
+            VowelCount = 0
+
+            for char in text:
+                 if char == vowel[i] :
+                     VowelCount += 1
+            print( vowel[i], VowelCount)
+
+text = "a chain is only as strong as its weakest link" # when using an input text code give a syntax error
+Reveal = VowelsCount(text)
+print (Reveal)
