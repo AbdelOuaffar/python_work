2044b3b607fe94c9732d4f8f70f2c20884b27b84
Noah <adel_chicago@yahoo.com>
2018-10-11 10:46:36 -0500

    Leapyear done differently

--- VowelsCount.py
+++ VowelsCount.py
@@ -7,7 +7,7 @@ def VowelsCount(text):
             for char in text:
                  if char == vowel[i] :
                      VowelCount += 1
-            print( vowel[i], VowelCount)
+            print(vowel[i], VowelCount)
 
 text = "a chain is only as strong as its weakest link" # when using an input text code give a syntax error
 Reveal = VowelsCount(text)
