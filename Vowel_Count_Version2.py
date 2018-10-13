b1bb5aadf1b1eb11deb3a1ec64578c7e68512da6
Noah <adel_chicago@yahoo.com>
2018-10-13 10:41:43 -0500

    program gives how many vowels in a text and if the sum of vowels is odd or even or no vowels exist

--- Vowel_Count_Version2.py
+++ Vowel_Count_Version2.py
@@ -1,10 +1,21 @@
 def  Vowel_count(Text):
     Vowel_dictionary ={"Vowels": ["A","a","I","i","O","o","U","u","E","e"]}
+    cnt = 0
     for vowel in Vowel_dictionary["Vowels"]:
+
             compter = 0
             for char in text :
                 if char == vowel:
                     compter += 1
+            cnt += compter
             print (vowel,compter)
-text =input( "Enter text:  ")
+
+    if  cnt % 2 == 0 and cnt != 0:
+         print (cnt, "number of vowels in txt is even ")
+    elif cnt % 2 != 0:
+         print (cnt," the number is odd ")
+    elif cnt == 0 :
+         print (" No vowels in you text")
+text =input("Enter Text  :")
+print(text)
 Vowel_count(text)
