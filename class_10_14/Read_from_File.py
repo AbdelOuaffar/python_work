82e9f877d1d3b07e4c82551397c026e9934cc2e4
Noah <adel_chicago@yahoo.com>
2018-10-19 19:23:46 -0500

    Read a file,and prints  data needed in stoutput

--- class_10_14/Read_from_File.py
+++ class_10_14/Read_from_File.py
@@ -1,12 +1,21 @@
-with open('C:/Users/PC/python_work/employees.txt', 'r') as f:
+try:
+    with open('C:/Users/PC/python_work/employees.txt', 'r') as f:
 
        headline=f.readline()
        line1=f.readline()
        head=headline.split()
        line=line1.split()
        print(head[0],int(line[0]))
-       print("FullName:",line[2]+","+line[1])
-       print(head[3],float(line[3]))
+       print( " FullName:" ,line[2]+" "+line[1] )
+       print( head[3], float(line[3]))
+except IOError :
+       print("an error Occured")
        f.close()
+
+
+
+
+
+
 
 
