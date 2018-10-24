50d5e4b371e66b9d3a9ff5ec04b15c6a2492d1f7
Noah <adel_chicago@yahoo.com>
2018-10-23 21:18:45 -0500

    Return string integer to position

---
+++ class_10_14/recursive_flat.py
@@ -0,0 +1,33 @@
+def flat(list):
+    if type(list[0]) != type([]):
+        return  list
+
+    final_list = []
+    for sub_list in list:
+        for element in sub_list:
+            final_list.append(element)
+
+    return final_list
+
+
+def flatN(list):
+    flattened_list = flat(list)
+    while type(flattened_list[0]) == type([]):
+        flattened_list = flat(flattened_list)
+
+    return flattened_list
+
+def flatN_recursive(list):
+    if type(list[0]) != type([]):
+        return list
+
+    final_list = []
+    for sub_list in list:
+        flattend = flat(sub_list)
+       final_list  += flatN_recursive(flattend)
+    return  final_list
+
+test_list = [[[[1, 2]], [[3, 4]]]]
+result = flatN_recursive(test_list)
+print(result)
+# [1,2,3,4]
---
+++ class_10_14/str_to_int.py
@@ -0,0 +1,19 @@
+import sys
+
+git stat
+def str_to_int(str):
+    multiplier = 1
+    for char in reversed(str):
+        print(f"{multiplier} - {char}")
+        multiplier *= 10
+
+
+def __main__():
+    if len(sys.argv) < 2:
+        print("Please pass an argument to convert to an integer.")
+        return
+
+    str_to_int(sys.argv[1])
+
+
+__main__()
