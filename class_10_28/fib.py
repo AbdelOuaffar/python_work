ac1d9033cb0506388739fb3213360af2c9af7858
Noah <adel_chicago@yahoo.com>
2018-10-26 21:31:05 -0500

    fib is executed through command line

--- class_10_28/fib.py
+++ class_10_28/fib.py
@@ -1,3 +1,4 @@
+import sys
 def fib(n):
     a = 0
     b = 1
@@ -7,11 +8,38 @@ def fib(n):
         b = x
         x = a + b
     return x
-n = int(input("enter a number: "))
-if n >= 0:
- result = fib(n)
- print(f"fib({n})= {result} ")
-else:
-    m = -n
-    result = -fib(m)
-    print(f"fib({n})= {result} ")
+def __main__():
+
+        if len(sys.argv) < 2:
+             print("enter an argument")
+             return
+        try:
+          n = int(sys.argv[1])
+          if n >= 0:
+                result = fib(n)
+          elif n < 0:
+                result = - fib(-n)
+          print(result)
+        except ValueError:
+
+                print("check if your input is numeric")
+        finally:
+            print("EOScript")
+__main__()
+
+
+
+
+
+
+
+
+
+#n = int(input("enter a number: "))
+#if n >= 0:
+ #result = fib(n)
+# print(f"fib({n})= {result} ")
+#else:
+   # m = -n
+    #result = -fib(m)
+   # print(f"fib({n})= {result} ")
