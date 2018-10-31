d6ce6667dfa563288945c07e966bc1a8d5317f36
Noah <adel_chicago@yahoo.com>
2018-10-31 10:24:31 -0500

    return Dollar amount in currency denomination. getting different result when I use sys.argv

--- November_04/money_denominat.py
+++ November_04/money_denominat.py
@@ -16,20 +16,24 @@ def bills_denominations(amount):
         amount = r
         if bill != 0:
           print(bill, key)
-amount = input("enter the amount  :")
+amount = sys.argv[1]
 Amount = float(amount)
 z = int(((Amount-int(Amount))+0.001) * 100)
 bills_denominations(int(Amount))
 coins_denominations(z)
-#Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
-#runfile('C:/Users/PC/python_work/November_04/money_denominat.py', wdir='C:/Users/PC/python_work/November_04')
-#enter the amount  :>? 123.45
-#1 hundreds
-#1 twenties
-#1 twos
-#1 ones
-#1 quarters
-#2 dimes
+
+# note:  same code, in the first version that was submitted it gives correct output
+# when I use sys.argv the coding give different results!!!!!!!!!!!!!!
+
+#(venv) C:\Users\PC\python_work\November_04>money_denominat.py 123.45
+#(24, 'fives')
+#(3, 'ones')
+#(9, 'nickles')
+
+#(venv) C:\Users\PC\python_work\November_04>money_denominat.py 2345.67
+#(469, 'fives')
+#(13, 'nickles')
+#(2, 'pennies')
 
 
 
