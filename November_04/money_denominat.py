5db153e8b8220c4c961394f157c7576c7ef7ab64
Noah <adel_chicago@yahoo.com>
2018-11-01 01:19:05 -0500

    final coding work for returning money denomination. result are ok when i don't use sys.argv

--- November_04/money_denominat.py
+++ November_04/money_denominat.py
@@ -9,32 +9,41 @@ def coins_denominations(amount):
         if coin != 0:
            print(coin, key)
 def bills_denominations(amount):
+
     bills = {"hundreds": 100, "twenties": 20, "tens": 10, "fives": 5, "twos": 2, "ones": 1}
     for key in bills:
         bill = amount // bills[key]
         r = amount % bills[key]
         amount = r
         if bill != 0:
-          print(bill, key)
+            print(bill, key)
 def main():
-    amount = sys.argv[1]
-    Amount = float(amount)
-    z = int(((Amount-int(Amount))+0.001) * 100)
-    bills_denominations(int(Amount))
-    coins_denominations(z)
+    try:
+        char_seq1 = ""
+        char_seq2 = ""
+        string = sys.argv[1]
+        for char in string:
+
+            if char != ".":
+                char_seq1 += char
+            elif char == ".":
+
+                char_seq2 = char_seq1
+                char_seq1 = ""
+
+        bills_denominations(int(char_seq2))
+        coins_denominations(int(char_seq1))
+    except TypeError:
+        print("type error")
 if __name__== "__main__":
     main()
 
+#(venv) C:\Users\PC\python_work\November_04>money_denominat.py 234.99
+#(46, 'fives')
+#(4, 'ones')
+#(19, 'nickles')
+#(4, 'pennies')
 
-(venv) C:\Users\PC\python_work\November_04>money_denominat.py 123
-(24, 'fives')
-(3, 'ones')
-
-(venv) C:\Users\PC\python_work\November_04>money_denominat.py 123.99
-(24, 'fives')
-(3, 'ones')
-(19, 'nickles')
-(4, 'pennies')
 
 
 
