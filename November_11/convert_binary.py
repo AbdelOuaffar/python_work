504dc1facc23aca5a7d8c124f496eebc51f70a26
Noah <adel_chicago@yahoo.com>
2018-11-08 12:11:13 -0600

    Chapter 17 summary,Binary conversion

---
+++ November_11/convert_binary.py
@@ -0,0 +1,23 @@
+def break_binary_single_digit(string):
+    list_binary_digits = []
+    for char in string:
+        list_binary_digits.append(int(char))
+    return list_binary_digits
+def convert_binary_decimal(binary_list):
+    decimal = 0
+    rev_list = []
+    rev_list += reversed(binary_list)
+    for i in range(len(binary_list)):
+        decimal += rev_list[i]*pow(2, i)
+
+    return decimal
+def main():
+
+    binary_string = input("enter a binary number")
+    binary_list = break_binary_single_digit(binary_string)
+    result = convert_binary_decimal(binary_list)
+    print(f"decimal number of {int(binary_string)} = {result}  ")
+if __name__=="__main__":
+    main()
+
+
