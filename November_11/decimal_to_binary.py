a84e8560ee2ee0d7e1cbaf1020edb072a4b66947
Noah <adel_chicago@yahoo.com>
2018-11-09 22:19:28 -0600

    eliminated recursion and using global var

--- November_11/decimal_to_binary.py
+++ November_11/decimal_to_binary.py
@@ -1,35 +1,30 @@
 def remainders(decimal):
-    quotient = decimal//2
-    rem = decimal % 2
-    list_remainders.append(rem)
-    decimal = quotient
-    if quotient == 1 and (rem == 0 or 1):
-        list_remainders.append(quotient)
-        return list_remainders.reverse()
-    return remainders(decimal)
+    list_remainders=[]
+    quotient = 2
+    while quotient != 1:
+        quotient = decimal//2
+        rem = decimal % 2
+        list_remainders.append(rem)
+        decimal = quotient
+    list_remainders.append(quotient)
+    list_remainders.reverse()
+    return list_remainders
+
 def binary_string(list):
     binary = ""
     for element in list:
         binary += str(element)
     return binary
 def main():
-    global list_remainders
-    list_remainders = []
     decimal = int(input("enter a decimal number"))
-    remainders(decimal)
-    result = binary_string(list_remainders)
+    list_rem = remainders(decimal)
+    result = binary_string(list_rem)
     print(f"binary number equivalent to {decimal} ={result}")
 if __name__=="__main__":
     main()
-#enter a decimal number>? 16
-#binary number equivalent to 16 =10000
 
-#enter a decimal number>? 5
-#binary number equivalent to 5 =101
+#enter a decimal number>? 20
+#binary number equivalent to 20 =10100
 
-
-#enter a decimal number>? 12
-#binary number equivalent to 12 =1100
-
-
-
+#enter a decimal number>? 8
+#binary number equivalent to 8 =1000
