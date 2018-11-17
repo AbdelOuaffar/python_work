3af02a000326533f41c8334913d188f9ed9c6a3a
Noah <adel_chicago@yahoo.com>
2018-11-16 22:47:09 -0600

    updated :created new function and iteration on the input

--- November_19/GCD.py
+++ November_19/GCD.py
@@ -9,19 +9,26 @@ def grand_c_d(a, b):
             b = r
     print(f"GCD({x},{y})={b}")
 
-def main():
-    a = int(input("enter first integer:"))
-    b = int(input("enter second integer"))
+def process(a,b):
     if a == 0 and b != 0:
         print(f"GCD({a},{b})={b}")
     elif a != 0 and b == 0:
         print(f"GCD({a},{b})={a}")
     elif a == 0 and b == 0:
-        print("there is none for (0,0)")
+        print("GDC(0,0)= none")
     elif a >= b:
         grand_c_d(a, b)
     else:
         grand_c_d(b, a)
+
+def main():
+    a = input("enter first integer:")
+    while a != "q":
+        a = int(a)
+        b = int(input("enter second integer"))
+        process(a, b)
+        a = input("enter an integer or(q) to quit  :")
+
 if __name__=="__main__":
     main()
 
